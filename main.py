c682b3dc0a6a59122079e4f54760defb9bf65ac8
Efe Basol <efebasol45@gmail.com>
2022-02-07 17:42:35 +0300

    ogrenci islemleri bitti

--- main.py
+++ main.py
@@ -1,40 +1,61 @@
-# mongodb
-from typing_extensions import Self
+# kütüphaneler
+from ipaddress import ip_address
+from openpyxl import load_workbook
 import pymongo
+# mongodb bağlantı
 mongoClient = pymongo.MongoClient("mongodb://localhost:27017/")
 mongoDB = mongoClient["test"]
 
+# Başlangıçta hangi işlemin yapılmasını seçme menüsü
 sayi = 0
 def secim():
+    # menüyü sürekli açık tutma
     while sayi < 1:
-        secim = input("secim: ")
+        print('''
+        (1) Öğrenci ekle
+        (2) Toplu öğrenci ekle
+        (3) Öğrenci sil
+        (4) Öğrenci görüntüle
+        (5) Çıkış     
+        ''')
+        secim = input('Secim yapınız: ')
         if secim == "1":
             sinif = input("Sınıf :")
             isim = input("İsim: ")
             soyisim = input("Soyad: ")
             numara = input("Numara :")
-            ogrenci = Ogrenci(sinif, isim, soyisim, numara)
+            ogrenci = Ogrenci(sinif, isim, soyisim, numara, None, None, None, None)
             ogrenci.kayit()
+        #! KUllanma çalışmıyor (TODO:satır 65)
         elif secim == "2":
-            sinif = input("Sınıf: ")
-            numara = input("Numara: ")
-            ogrenci = Ogrenci(sinif, None, None, numara)
-            ogrenci.sil()
+            pass
         elif secim == "3":
             sinif = input("Sınıf: ")
             numara = input("Numara: ")
-            ogrenci = Ogrenci(sinif, None, None, numara)
+            ogrenci = Ogrenci(sinif, None, None, numara, None, None, None, None)
+            ogrenci.sil()
+        elif secim == "4":
+            sinif = input("Sınıf: ")
+            numara = input("Numara: ")
+            ogrenci = Ogrenci(sinif, None, None, numara, None, None, None, None)
             ogrenci.gorntuleme()
-        elif secim == "cikis":
+        elif secim == "5":
             break
 
+#* Ogrenci ile ilgili tüm işlmeler
 class Ogrenci:
     def __init__(self, sinif, isim, soyisim, numara):
         self.sinif = sinif
         self.isim = isim
         self.soyisim = soyisim
         self.numara = numara
+        #* coklu_kayit fonksiyonu için gereken methodlar şimdilik kapalı classtan silindi
+        #self.satir = satir
+        #self.sutun = sutun
+        #self.workbook = workbook
+        #self.maxSatir = maxSatir
 
+    # Ogrenci sinif, numara, isim, soyisim girerek öğrenci kaydetme (her seferinde sadece bir kişi kayıt edilebilir)
     def kayit(self):
         if self.sinif == "m":
             mongoCol = mongoDB["m"]
@@ -46,9 +67,11 @@ class Ogrenci:
             mongoCol.insert_one(data)
             print(f"{self.numara}'lı öğrenci kayıt edildi!")
 
+    #TODO: Çoklu kayıt yapılamadı en son tekrar dene
     def coklu_kayit(self):
         pass
 
+    # Ogrenci numarsı ile öğrenci silme
     def sil(self):
         if self.sinif == "m":
             mongoCol = mongoDB["m"]
@@ -58,6 +81,7 @@ class Ogrenci:
             mongoCol.delete_one(data)
             print(f"{self.numara}'lı öğrenci silindi!")
 
+    # Ogrenci numarası ile öğrenci bilgilerini görme
     def gorntuleme(self):
         if self.sinif == "m":
             mongoCol = mongoDB["m"]
@@ -65,9 +89,8 @@ class Ogrenci:
                 '_id': self.numara
             }
             x = mongoCol.find_one(data)
-            print(x)
+            print(x)   
 class Sinav:
     pass
-
 
 secim()
